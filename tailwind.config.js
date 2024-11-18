2216cf010ce0cff57e2fa981449a3ac9b60229e0
sceptzrion <ikhsan.ry@gmail.com>
2024-11-18 21:40:19 +0700

    Create navbar, hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./*.{html,js,php}"],
   theme: {
+    colors: {
+      'light': '#ffffff',
+      'dark': '#000',
+      'gray': '#D9D9D9',
+      // linear gradient colors
+      'green': '#299E63',
+      'dark-green': '#0F3823',
+    },
     extend: {},
   },
   plugins: [
